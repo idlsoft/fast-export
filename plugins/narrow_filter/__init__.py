6e5eaae584d7499b59b7cd6efdfc2b716427d8f1
Sandu Turcan <idlsoft@gmail.com>
2022-12-07 15:04:20 -0500

    narrow_filter: convert .hgignore to .gitignore

--- plugins/narrow_filter/__init__.py
+++ plugins/narrow_filter/__init__.py
@@ -20,6 +20,8 @@ class Filter:
     def file_name_filter(self, filename):
         if b'/out/production/' in filename or b'/out/test/' in filename:
             return ''
+        if filename == b'.hgignore':
+            return b'.gitignore'
         if self.root:
             if not filename.startswith(self.root):
                 return ''
@@ -30,3 +32,45 @@ class Filter:
                     return filename
             filename = ''
         return filename
+
+    def file_data_filter(self, file_data):
+        if file_data['filename'] == b'.hgignore':
+            data = file_data['data']
+            lines = []
+            for line in data.split(b'\n'):
+                if line == b'syntax: glob' or line == '.git/':
+                    continue
+                if line.startswith(b'src/'):
+                    line = b'**/' + line
+                lparts = line.split(b'/')
+                if len(lparts) > 1:
+                    if lparts[-1] == b'*':
+                        line = line[:-1]
+                        if len(lparts) == 3 and lparts[0] == b'*':
+                            line = line[2:]
+                            lparts[0] = b''
+                    if lparts[0] == b'*':
+                        line = b'*' + line
+                    if line.startswith(b'**/') and (len(lparts) == 2 or (len(lparts) == 3 and line.endswith(b'/'))):
+                        line = line[3:]
+                if line.startswith(b'rootglob:'):
+                    line = b'/' + line[9:]
+                if not line.startswith(b'*'):
+                    lparts = line.split(b'/')
+                    if len(lparts) > 2 or line.startswith(b'/'):
+                        if line.startswith(b'/'):
+                            line = line[1:]
+                        if self.root:
+                            if line.startswith(self.root):
+                                line = b'/' + line[len(self.root):]
+                            else:
+                                continue
+                        if self.prefixes:
+                            for prefix in self.prefixes:
+                                if line.startswith(prefix):
+                                    line = b'/' + line
+                                    break
+                            else:
+                                continue
+                lines.append(line)
+            file_data['data'] = b'\n'.join(lines)
